84c1202df25189b2fd4d735d2d1ba8bc349ca12a
Kaveen Kumarasinghe <ocfinancesmc@gmail.com>
2018-10-07 19:33:07 -0400

    Getters and Setters

--- src/me/kaveenk/binarytree/MyBinaryTree.java
+++ src/me/kaveenk/binarytree/MyBinaryTree.java
@@ -3,6 +3,25 @@ package me.kaveenk.binarytree;
 public class MyBinaryTree {
 	private int numInTree;
 	private StudentInfo root;
-	
+
+	/* Start Getters and Setters */
+	/* Getters */
+	public StudentInfo getRoot() {
+		return this.root;
+	}
+
+	public int getNumInTree() {
+		return this.numInTree;
+	}
+
+	/* Setters */
+	public void setRoot(StudentInfo root) {
+		this.root = root;
+	}
+
+	public void setNumInTree(int numInTree) {
+		this.numInTree = numInTree;
+	}
+	/* End Getters and Setters */
 
 }
--- src/me/kaveenk/binarytree/StudentInfo.java
+++ src/me/kaveenk/binarytree/StudentInfo.java
@@ -6,5 +6,53 @@ public class StudentInfo {
 	private String lastName;
 	private StudentInfo left;
 	private StudentInfo right;
+	
+    /* Start Getters and Setters */
+	/* Getters */
 
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public int getStudentNum() {
+		return this.studentNum;
+	}
+
+	public StudentInfo getLeft() {
+		return this.left;
+	}
+
+	public StudentInfo getRight() {
+		return this.right;
+	}
+	/* Setters */
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setStudentNum(int studentNum) {
+		this.studentNum = studentNum;
+	}
+
+	public void setLeft(StudentInfo left) {
+		this.left = left;
+	}
+
+	public void setRight(StudentInfo right) {
+		this.right = right;
+	}
+	/* End Getters and Setters */
+
+	
+	
+	
 }
